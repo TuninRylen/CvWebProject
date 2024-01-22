79e766da8df683c86c6a9890f724d241fb38ca19
TuninRylen <132095456+TuninRylen@users.noreply.github.com>
2024-01-22 20:29:00 +0300

    v3

---
+++ CvProject/CvProject/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CvProject.Models.Entity;
+
+
+namespace CvProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbCvEntities db = new DbCvEntities();
+        
+        // GET: Default
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
+        }
+
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
+    }
+}
