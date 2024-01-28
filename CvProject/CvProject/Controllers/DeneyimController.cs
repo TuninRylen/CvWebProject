54a0a3dccd7d280189e23335765d3b4ec1f07eba
TuninRylen <132095456+TuninRylen@users.noreply.github.com>
2024-01-28 17:49:01 +0300

    v9

---
+++ CvProject/CvProject/Controllers/DeneyimController.cs
@@ -0,0 +1,21 @@
+﻿using CvProject.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CvProject.Controllers
+{
+    public class DeneyimController : Controller
+    {
+        DeneyimRepository repo = new DeneyimRepository();
+
+        // GET: Deneyim
+        public ActionResult Index()
+        {
+            var degerler = repo.List();
+            return View(degerler);
+        }
+    }
+}
---
+++ CvProject/CvProject/Repositories/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using CvProject.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CvProject.Repositories
+{
+    public class GenericRepository<T> where T : class, new()
+    {
+        DbCvEntities db = new DbCvEntities();
+
+        public List<T> List()
+        {
+            return db.Set<T>().ToList();
+        }
+
+        public void Add(T entity)
+        {
+            db.Set<T>().Add(entity);
+            db.SaveChanges();
+        }
+
+        public void Delete(T entity) 
+        {
+            db.Set<T>().Remove(entity);
+            db.SaveChanges();
+        }
+
+        public T Get(T entity) 
+        {
+            return db.Set<T>().Find(entity); 
+        }
+
+        public void Update(T entity) 
+        {
+            db.SaveChanges();
+        }
+
+    }
+
+
+}
