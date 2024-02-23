8104fa5ff0866ac3d4016c82c882548eddbfa850
TuninRylen <132095456+TuninRylen@users.noreply.github.com>
2024-02-23 17:20:09 +0300

    v14

---
+++ CvProject/CvProject/Controllers/EgitimController.cs
@@ -0,0 +1,21 @@
+﻿using CvProject.Models.Entity;
+using CvProject.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CvProject.Controllers
+{
+    public class EgitimController : Controller
+    {
+        GenericRepository<TblEgitimlerim> repo = new GenericRepository<TblEgitimlerim>();
+
+        public ActionResult Index()
+        {
+            var egitim = repo.List();
+            return View(egitim);
+        }
+    }
+}
