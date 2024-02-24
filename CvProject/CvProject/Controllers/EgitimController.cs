f10cf84efcd501fc2f3c8e2f38c18f15b43fb37c
TuninRylen <132095456+TuninRylen@users.noreply.github.com>
2024-02-24 17:12:04 +0300

    v15

--- CvProject/CvProject/Controllers/EgitimController.cs
+++ CvProject/CvProject/Controllers/EgitimController.cs
@@ -17,5 +17,50 @@ namespace CvProject.Controllers
             var egitim = repo.List();
             return View(egitim);
         }
+
+        [HttpGet]
+        public ActionResult EgitimEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EgitimEkle(TblEgitimlerim T)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimEkle");
+            }
+
+            repo.Add(T);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult EgitimSil(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            repo.Delete(egitim);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EgitimDuzenle(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            return View(egitim);
+        }
+
+        [HttpPost]
+        public ActionResult EgitimDuzenle(TblEgitimlerim T)
+        {
+            var egitim = repo.Find(x => x.ID == T.ID);
+            egitim.Baslik = T.Baslik;
+            egitim.AltBaslik1 = T.AltBaslik1;
+            egitim.AltBaslik2 = T.AltBaslik2;
+            egitim.Tarih = T.Tarih;
+            egitim.GNO = T.GNO;
+            repo.Update(egitim);
+            return RedirectToAction("Index");
+        }
     }
 }
