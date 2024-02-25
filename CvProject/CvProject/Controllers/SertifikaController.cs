1a7fa5a9881d71cdbbe352c491c7f17943b92a90
TuninRylen <132095456+TuninRylen@users.noreply.github.com>
2024-02-25 14:17:32 +0300

    v16

---
+++ CvProject/CvProject/Controllers/SertifikaController.cs
@@ -0,0 +1,59 @@
+﻿using CvProject.Models.Entity;
+using CvProject.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CvProject.Controllers
+{
+    public class SertifikaController : Controller
+    {
+
+        GenericRepository<TblSertifikalarım> repo = new GenericRepository<TblSertifikalarım>();
+
+        public ActionResult Index()
+        {
+            var sertifika = repo.List();
+            return View(sertifika);
+        }
+
+        [HttpGet]
+        public ActionResult SertifikaEkle() 
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SertifikaEkle(TblSertifikalarım T)
+        {
+            repo.Add(T);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SertifikaSil(int id)
+        {
+            var sertifika = repo.Find(x => x.ID == id);
+            repo.Delete(sertifika);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult SertifikaDuzenle(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            return View(egitim);
+        }
+
+        [HttpPost]
+        public ActionResult SertifikaDuzenle(TblSertifikalarım T)
+        {
+            var sertifika = repo.Find(x => x.ID == T.ID);
+            sertifika.Aciklama = T.Aciklama;
+            repo.Update(sertifika);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- CvProject/CvProject/Models/Entity/TblEgitimlerim.cs
+++ CvProject/CvProject/Models/Entity/TblEgitimlerim.cs
@@ -11,12 +11,10 @@ namespace CvProject.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class TblEgitimlerim
     {
         public int ID { get; set; }
-        [Required(ErrorMessage ="Bu alaný boþ geçemezsiniz.")]
         public string Baslik { get; set; }
         public string AltBaslik1 { get; set; }
         public string AltBaslik2 { get; set; }
