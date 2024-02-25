7283783fc6db0b9a762cf08b44a85ad642331cff
TuninRylen <132095456+TuninRylen@users.noreply.github.com>
2024-02-25 20:02:51 +0300

    v17

---
+++ CvProject/CvProject/Controllers/HobilerimController.cs
@@ -0,0 +1,34 @@
+﻿using CvProject.Models.Entity;
+using CvProject.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CvProject.Controllers
+{
+    public class HobilerimController : Controller
+    {
+        GenericRepository<TblHobilerim> repo = new GenericRepository<TblHobilerim>();
+
+        public ActionResult Index()
+        {
+            var hobiler = repo.List();
+            return View(hobiler);
+        }
+
+        [HttpGet]
+        public ActionResult HobiEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult HobiEkle(TblHobilerim T)
+        {
+            repo.Add(T);  
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CvProject/CvProject/Controllers/IletisimController.cs
@@ -0,0 +1,22 @@
+﻿using CvProject.Models.Entity;
+using CvProject.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CvProject.Controllers
+{
+    public class IletisimController : Controller
+    {
+
+        GenericRepository<Tbliletisim> repo = new GenericRepository<Tbliletisim>();
+
+        public ActionResult Index()
+        {
+            var iletisim = repo.List();
+            return View(iletisim);
+        }
+    }
+}
